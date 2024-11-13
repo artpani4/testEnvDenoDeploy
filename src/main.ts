088b3c0b08c127e1f5dd1cd88d6170446e21efce
Artem <artp80@gmail.com>
2024-11-13 23:40:54 +0700

    fix6

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // const a = Deno.env.toObject()
 Deno.serve(() => {
-    console.log("AAAA")
+    // console.log("AAAA")
+    console.log(Deno.env.toObject())
     return new Response("Hello, world!")});
 // console.log(a)
